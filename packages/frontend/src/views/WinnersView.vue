4e41a84e309b134baf3449e6ca9648d709e86e06
Alexandre Cavalheiro S. Tiago da Silva <contact@thewizard.link>
2025-03-15 23:59:07 -0300

    feat(frontend/WinnersView): show extra information about the top three contestants

--- packages/frontend/src/views/WinnersView.vue
+++ packages/frontend/src/views/WinnersView.vue
@@ -1,15 +1,31 @@
 <script setup lang="ts">
-import { Api, type Vote } from "@/api";
+import { Api, type Cosplayer, type Vote } from "@/api";
 import { ref } from "vue";
 
 const votes = ref<Vote[]>([]);
+const dataFromWinners = ref<Cosplayer[]>([]);
 
-Api.getWinners().then((c) => (votes.value = c));
+const getWinnerData = (index: number): Cosplayer | undefined => {
+  return dataFromWinners.value[index];
+};
+
+Api.getWinners().then((w) => {
+  votes.value = w;
+
+  [0, 1, 2].forEach((i) => {
+    Api.getCosplayers({ name: w[i].name })
+      .then((c) => {
+        dataFromWinners.value[i] = c;
+        w[i].name = [undefined, null, ""].includes(c.nickname) ? c.name : c.nickname;
+      })
+      .catch(console.error);
+  });
+});
 </script>
 
 <template>
   <v-container>
-    <v-row class="cosplayerRow" v-for="vote in votes" :key="vote.name">
+    <v-row class="cosplayerRow" v-for="(vote, index) in votes" :key="vote.name">
       <v-sheet rounded>
         <v-container>
           <v-row>
@@ -21,6 +37,18 @@ Api.getWinners().then((c) => (votes.value = c));
             <v-col>
               <h5 class="text-h5">Pontuação total</h5>
               {{ vote.score }}
+            </v-col>
+          </v-row>
+
+          <v-row v-if="index <= 2">
+            <v-col>
+              <h5 class="text-h5">Personagem</h5>
+              {{ getWinnerData(index)?.characterName }}
+            </v-col>
+
+            <v-col>
+              <h5 class="text-h5">Origem</h5>
+              {{ getWinnerData(index)?.origin }}
             </v-col>
           </v-row>
         </v-container>
